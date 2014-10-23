aadad834da1ddd1ecb550c39f375abcba838c46c
camiloaguilar91 <cgaguila@uci.edu>
2014-10-23 15:48:41 -0700

    Made ProcessData a script instead of a function

--- matlab_fft/Emotiv/ProcessData.m
+++ matlab_fft/Emotiv/ProcessData.m
@@ -6,32 +6,32 @@
 % Modification History:                         %
 % 19/10/14 CA Initial Version                   % 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Main Script-Variables Definition
+%%
+%Load and Segment the Data
+clear;
+WindowLength = 0.5;
+EpochLength = 3;
+freq = 11;
+disp('Loading Data...');
+data2 = SegmentData('camilo_eeg_action_partII.csv', WindowLength, EpochLength);
+%data2 = SegmentData('camilo_rest.CSV', WindowLength, EpochLength);
+SegData = data2;%cat(3,data1,data2);
+clear data2
+
+disp('SUCCES: Data Loaded and Segmented');
+
+%%
+%Function to Low Pass Filter
 
 
-function[] = ProcessData(WindowLength, EpochLength, freq)
-    %Main Script-Variables Definition
-    %%
-    %Load and Segment the Data
-    disp('Loading Data...');
-    %data1 = SegmentData('camilo_eeg_action_partI.csv', WindowLength, EpochLength);
-    data2 = SegmentData('Omar_pilot_runII.CSV', WindowLength, EpochLength);
-    SegData  = data2;%cat(3,data1,data2);
-    clear data1 data2;
+%%
+%Get the FFT and power of data 
+disp('FFTing the data...')
+trialChannelPower = FFTPower(SegData, WindowLength, EpochLength);
+disp('SUCCESS: Data FFTED!');
 
-    disp('SUCCES: Data Loaded and Segmented');
-
-    %%
-    %Function to Low Pass Filter
-
-
-    %%
-    %Get the FFT and power of data 
-    disp('FFTing the data...');
-    trialChannelPower = FFTPower(SegData, WindowLength, EpochLength);
-    disp('SUCCESS: Data FFTED!');
-
-    %%
-    %Plot Specific Frequency
-    disp('Plotting Data');
-    PlotFreq(trialChannelPower, freq, WindowLength, EpochLength);
-end
+%%
+%Plot Specific Frequency
+disp('Plotting Data');
+PlotFreq(trialChannelPower, freq, WindowLength, EpochLength);
