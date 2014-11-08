df0bac5024d7421a08dd8241037b3286007e999b
oshanta <oashanta@gmail.com>
2014-11-08 14:15:12 -0800

    Arduino prgm for decision squeezing

---
+++ Arduino/Flash_action/Flash_decision/Flash_decision.ino
@@ -0,0 +1,36 @@
+int start_led = 13;
+int led1 = 12;
+int led2 = 11;
+int random_num;
+
+void setup() {
+  // initialize serial:
+  Serial.begin(9600);
+  pinMode(start_led, OUTPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);       
+}
+
+void loop() {
+  // print the string when a newline arrives:
+  digitalWrite(start_led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);               // wait for a second
+  digitalWrite(start_led, LOW);    // turn the LED off by making the voltage LOW
+  delay(2000);               // wait for a second
+  random_num = random(0,2);
+  if (random_num == 0)
+  {
+    digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
+    Serial.println(1); 
+    delay(500);               // wait for a second
+    digitalWrite(led1, LOW);    // turn the LED off by making the voltage LOW
+    delay(7000);               // wait for a second
+  }
+  else
+  {
+    digitalWrite(led2, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(500);               // wait for a second
+    digitalWrite(led2, LOW);    // turn the LED off by making the voltage LOW
+    delay(8000);               // wait for a second
+  }
+}
